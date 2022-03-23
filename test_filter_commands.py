5654b53aa57211f6797c480ccface0c5f02134bc
Antonio Gonzalez <antgonza@gmail.com>
2022-03-23 09:55:14 -0600

    rm some prints

--- test_filter_commands.py
+++ test_filter_commands.py
@@ -24,9 +24,7 @@ class TestPreProcessing(unittest.TestCase):
                                    self.db_name,
                                    self.output_path])
         print('======>')
-        print('======>')
         print(res_cmnd.communicate())
-        print('======>')
         print('======>')
         self.assertTrue(res_cmnd.returncode == 0)
 
