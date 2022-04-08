20f586c4e2f7590e740de0a531b1c2905c534808
cameronmartino <cameronmartino@gmail.com>
2022-04-07 22:36:45 -0700

    two step update

--- test-command.sh
+++ test-command.sh
@@ -3,12 +3,13 @@
 filename1=$1
 filename2=$2
 db_path=$3
-db_name=$4
-final_output=$5
+db_name_one=$4
+db_name_two=$5
+final_output=$6
 
 NPROCS=12
 
 filename1_short="test_res_R1"
 filename2_short="test_res_R2"
 
-fastp --adapter_sequence GATCGGAAGAGCACACGTCTGAACTCCAGTCAC --adapter_sequence_r2 GATCGGAAGAGCGTCGTGTAGGGAAAGGAGTGT -l 25 -i $filename1 -I $filename2 -w $NPROCS --stdout | minimap2 -ax sr -t $NPROCS $db_path/$db_name.mmi - -a | samtools fastq -@ $NPROCS -f 12 -F 256 -1 $final_output/${filename1_short}.trimmed.fastq.gz -2 $final_output/${filename2_short}.trimmed.fastq.gz
+fastp --adapter_sequence GATCGGAAGAGCACACGTCTGAACTCCAGTCAC --adapter_sequence_r2 GATCGGAAGAGCGTCGTGTAGGGAAAGGAGTGT -l 25 -i $filename1 -I $filename2 -w $NPROCS --stdout | minimap2 -ax sr -t $NPROCS $db_path/$db_name_one.mmi - -a | samtools fastq -@ $NPROCS -f 12 -F 256 | minimap2 -ax sr -t $NPROCS $db_path/$db_name_two.mmi - -a | samtools fastq -@ $NPROCS -f 12 -F 256 -1 $final_output/${filename1_short}.trimmed.fastq.gz -2 $final_output/${filename2_short}.trimmed.fastq.gz
--- test_filter_commands.py
+++ test_filter_commands.py
@@ -11,7 +11,8 @@ class TestPreProcessing(unittest.TestCase):
         self.file_one = self.output_path + "/all_reads_R1.fq"
         self.file_two = self.output_path + "/all_reads_R2.fq"
         self.database_path = self.curr_path
-        self.db_name = "human-phix-db"
+        self.db_one_name = "human-phix-db"
+        self.db_two_name = "chm13"
         pass
 
     def test_command(self):
@@ -21,7 +22,8 @@ class TestPreProcessing(unittest.TestCase):
                                    self.file_one,
                                    self.file_two,
                                    self.database_path,
-                                   self.db_name,
+                                   self.db_one_name,
+                                   self.db_two_name,
                                    self.output_path])
         self.assertTrue(res_cmnd.returncode == 0)
 
